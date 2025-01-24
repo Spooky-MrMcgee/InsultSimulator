543e092f13bb40ba961325a6a52e1bafc5faeed1
Elliot <72456517+Spooky-MrMcgee@users.noreply.github.com>
2025-01-24 13:27:53 +0000

    Score animation

--- Assets/Scripts/UI/PersistentUIManager.cs
+++ Assets/Scripts/UI/PersistentUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +12,8 @@ public class PersistentUIManager : MonoBehaviour
 
     [SerializeField] PlayerRoundCounter player1Rounds, player2Rounds;
 
+    [SerializeField] int player1CurrentScore = 0, player2CurrentScore = 0;
+
     private void Awake()
     {
         Instance = this;
@@ -23,7 +26,27 @@ public class PersistentUIManager : MonoBehaviour
 
     public void SetPlayer1Score(int score)
     {
-        player1Score.SetText(score.ToString());
+        StartCoroutine(IncrementPlayer1Score(score));
+    }
+
+    IEnumerator IncrementPlayer1Score(int newScore)
+    {
+        float timer = 0;
+
+        while(timer < 1)
+        {
+            player1Score.SetText(((int)Mathf.Lerp(player1CurrentScore, newScore, EaseOutQuint(timer))).ToString());
+            timer += 0.02f;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        player1CurrentScore = newScore;
+        player1Score.SetText(player1CurrentScore.ToString());
+
+        float EaseOutQuint(float time)
+        {
+            return 1 - Mathf.Pow(1 - time, 5);
+        }
     }
 
     public void SetPlayer1RoundsWon(int rounds)
@@ -38,7 +61,26 @@ public class PersistentUIManager : MonoBehaviour
 
     public void SetPlayer2Score(int score)
     {
-        player2Score.SetText(score.ToString());
+        StartCoroutine(IncrementPlayer2Score(score));
+    }
+
+    IEnumerator IncrementPlayer2Score(int newScore)
+    {
+        float timer = 0;
+
+        while (timer < 1)
+        {
+            player2Score.SetText(((int)Mathf.Lerp(player2CurrentScore, newScore, EaseOutQuint(timer))).ToString());
+            timer += 0.02f;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        player2CurrentScore = newScore;
+        player2Score.SetText(player2CurrentScore.ToString());
+
+        float EaseOutQuint(float time) {
+            return 1 - Mathf.Pow(1 - time, 5);
+        }
     }
 
     public void SetPlayer1Turn(bool on)
