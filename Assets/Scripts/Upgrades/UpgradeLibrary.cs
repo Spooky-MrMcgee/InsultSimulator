1a928c47aa823c55f810450ab9290356699de502
Elliot <72456517+Spooky-MrMcgee@users.noreply.github.com>
2025-01-24 12:48:40 +0000

    Adding upgrade library

--- Assets/Scripts/Upgrades/UpgradeLibrary.cs
+++ Assets/Scripts/Upgrades/UpgradeLibrary.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeLibrary : MonoBehaviour
+[CreateAssetMenu(menuName = "Library/UpgradeLibrary")]
+public class UpgradeLibrary : ScriptableObject
 {
     public List<UpgradeCard> upgrades;
 }
